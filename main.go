7d9ee92cc0198667f8201ef07b7be2cbe450f176
Michael Miranda <m.miranda1219@gmail.com>
2020-02-23 15:12:37 -0500

    adds story caching

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		stories, err := getTopStories(numStories)
+		stories, err := getCachedStories(numStories)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -48,6 +48,27 @@ func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 			return
 		}
 	})
+}
+
+var (
+	cache           []item
+	cacheExpiration time.Time
+)
+
+func getCachedStories(numStories int) ([]item, error) {
+	// now < x == now.Sub(x) < 0
+	if time.Now().Sub(cacheExpiration) < 0 {
+		return cache, nil
+	}
+
+	stories, err := getTopStories(numStories)
+	if err != nil {
+		return nil, err
+	}
+
+	cache = stories
+	cacheExpiration = time.Now().Add(1 * time.Second)
+	return cache, nil
 }
 
 func getTopStories(numStories int) ([]item, error) {
@@ -69,7 +90,6 @@ func getTopStories(numStories int) ([]item, error) {
 }
 
 func getStories(ids []int) []item {
-	var client hn.Client
 	type result struct {
 		index int
 		item  item
@@ -79,6 +99,7 @@ func getStories(ids []int) []item {
 
 	for i := 0; i < len(ids); i++ {
 		go func(index, id int) {
+			var client hn.Client
 			hnItem, err := client.GetItem(id)
 			if err != nil {
 				resultCh <- result{index: index, err: err}
